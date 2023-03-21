282896cbb3b7660f4d571ecbf8ddb0057017b82a
zhanghong <hong.zhang@axinan.com>
2023-03-21 13:25:23 +0800

    FEAT: support form

--- src/main.js
+++ src/main.js
@@ -6,35 +6,36 @@ import message from 'antd/es/message';
 import 'antd/es/input/style';
 import 'antd/es/message/style';
 import 'antd/es/tag/style';
-import './index.css'
-const Main = (props, ref) => {
-  const [value, setValue] = useState(props.value)
+import './index.css';
+
+const Main = (props) => {
+  const { value, onChange } = props;
   const [valueInput, setValueInput] = useState('')
   const inputRef = useRef(null);
-  useImperativeHandle(ref, () => ({
-    changeVal: () => {
-      return value
-    }
-  }));
+
   function pressEnter(e) {
     if (e.target.value) {
-      setValue([...value, e.target.value])
-      setValueInput('')
+      onChange([...value, e.target.value]);
+      setValueInput('');
     } else {
-      message.error('无数据')
+      message.error('No input text');
     }
   }
+
   function preventDefault(str, e) {
     e.preventDefault();
-    setValue(value.filter(item => item !== str))
+    onChange(value.filter(item => item !== str));
   }
+
   function focus() {
     inputRef.current && inputRef.current.focus()
   }
+
   function handleChange(e) {
     let elm = e.target;
     setValueInput(elm.value)
   }
+
   // 按下删除监听
   function keyDown(e) {
     if (e.keyCode === 8 && !valueInput) {
@@ -43,6 +44,7 @@ const Main = (props, ref) => {
       }))
     }
   }
+  
   return (
     <div>
       <div onClick={focus} className='wrap'>
@@ -65,9 +67,5 @@ const Main = (props, ref) => {
 
 Main.propTypes = {
   value: PropTypes.array.isRequired,
+  onChange: PropTypes.func,
 };
-Main.defaultProps = {
-  value: []
-};
-
-export default forwardRef(Main)
